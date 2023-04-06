1dd0b236f974797f45c06a46e03f9239c9de8d6d
faruk <kursunomerfaruk2@gmail.com>
2023-04-06 13:12:34 +0200

    refs, component

---
+++ 14-vue-cli/neu-project/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h3> {{title}} </h3>
+    <input type="text" ref="name">
+    <br> <br>
+    <input type="text" ref="password">
+    <br> <br>
+    <button @click="login">Giris</button>
+    <br>
+    <p v-if="isLogin"> Gris Basarili  </p>
+    <p v-else> Gris Basarisiz  </p>
+  </div>
+
+  
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+    return{
+      title:'login',
+      name:'Varol',
+      password:'1234',
+      isLogin:true
+
+    }
+  },
+  methods: {
+    login(){
+      // console.log(this.$refs.password.value);
+      return this.$refs.name.value === this.name && this.$refs.password.value === this.password ? this.isLogin = true : this.isLogin = false
+      
+
+    }
+   
+
+  }
+ 
+  
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 14-vue-cli/vue-component/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <Title :titleText="text" :text="text2"/>
+    <h3>App Vue Baslik</h3>
+  </div>
+</template>
+
+<script>
+
+import Title from './components/Title-Vue'
+export default {
+  name: 'App',
+  components:{
+    Title
+  },
+  data() {
+    return {
+      text:'Vue 3 giris',
+      text2:'Vue text deneme'
+    }
+  },
+  
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
