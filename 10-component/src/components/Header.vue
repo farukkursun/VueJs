296781f71381a5485f68a7b22b9c5a1771d96f23
faruk <kursunomerfaruk2@gmail.com>
2023-04-09 13:20:21 +0200

    watch

--- 10-component/src/components/Header.vue
+++ 10-component/src/components/Header.vue
@@ -7,6 +7,10 @@
     <input type="text" v-model="name">
     <input type="text" v-model="surname">
     <p> {{nameSurname}} </p>
+    <hr>
+    <input type="number" v-model="number1"> <br>
+    <input type="number" v-model="number2"> <br>
+    <input type="number" v-model="total">
   </div>
 </template>
 
@@ -16,8 +20,21 @@ export default {
     data() {
         return {
             name:'',
-            surname:''
+            surname:'',
+            number1:0,
+            number2:0,
+            total:0
         }
+    },
+    watch:{
+        number1(newValue, oldValue){
+            console.log('sayilar', newValue, oldValue);
+            this.total = parseInt(this.number1) + parseInt(this.number2)
+        },
+        number2(newValue, oldValue){
+            console.log('sayilar', newValue, oldValue);
+            this.total = parseInt(this.number1) + parseInt(this.number2)
+        },
     },
     computed: {
         nameSurname(){
