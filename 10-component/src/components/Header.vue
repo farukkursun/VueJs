46e829010ebaacce26836b25118df0878b2ebda8
faruk <kursunomerfaruk2@gmail.com>
2023-04-09 12:56:42 +0200

    slot

---
+++ 10-component/src/components/Header.vue
@@ -0,0 +1,19 @@
+// slot ile header componentine gönderilecek verileri props ile almadan alabiliyoruz. Bunun icin gönderilen yede kompenent etiket olarak yaziliyor arasina yazilan her sey slot ile aliniyor
+<template>
+  <div>
+    <slot></slot>
+
+    <!-- <h1> {{content}} </h1> -->
+  </div>
+</template>
+
+<script>
+export default {
+    // props:['content']
+
+}
+</script>
+
+<style>
+
+</style>
