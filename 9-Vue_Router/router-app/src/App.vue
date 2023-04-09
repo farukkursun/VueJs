5d8d626eb8522c77f4925478bf61e8187379a1d0
faruk <kursunomerfaruk2@gmail.com>
2023-04-09 11:06:49 +0200

    router go, push

--- 9-Vue_Router/router-app/src/App.vue
+++ 9-Vue_Router/router-app/src/App.vue
@@ -4,17 +4,40 @@
       <!-- <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> -->
       <!-- yukarida reuterin path ile kullanimi var name ile de kullanilabilir index.js de her iki örnege yer verilmis asagida name ile örnrk yapacagiz -->
-      <router-link :to="{name:'home'}">Home</router-link> |
+      <router-link :to="{name:'home'}">Home</router-link> | 
       <router-link :to="{name:'about'}">About</router-link> |
       <router-link :to="{name:'students'}">Students</router-link>
       <!-- baskayol -->
       <!-- <router-link :to="{path:'/'}">Home</router-link> |
       <router-link :to="{path:'/about'}">About</router-link> -->
   </nav>
+  <div>
+    <span @click="anasayfa">Anasayfa</span>
+    <span @click="geri">Geri</span>
+    <span @click="ileri"> Ileri</span>
+  </div>
   <router-view/>
 </div>
  
 </template>
+
+<script>
+  export default {
+    methods: {
+      anasayfa(){
+        this.$router.push({name:'home'})
+
+      },
+      geri(){
+        this.$router.go(-1)
+      },
+      ileri(){
+        this.$router.go(1)
+      }
+      
+    },
+  }
+</script>
 
 <style>
 #app {
@@ -36,5 +59,9 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+span{
+  padding: 20px 10px;
+  cursor: pointer;
 }
 </style>
