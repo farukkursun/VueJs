352ab207a0c4f64eb95138c9a1ad48faa3483151
faruk <kursunomerfaruk2@gmail.com>
2023-04-17 14:59:04 +0200

    setup, ref templete

---
+++ 12-compositions-api/src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+  <p> {{name}} - {{job}} </p>
+  <p ref="info">Deneme</p>
+  <button @click="clicked"> click</button>
+    
+  </div>
+</template>
+
+<script>
+import {ref} from 'vue';
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  },
+
+  setup() {
+    const name ='faruk';
+    const job = 'developer';
+
+    const info = ref(null)
+
+    const clicked = ()=>{
+      // console.log('tiklandi');
+      console.log(info, info.value);
+    }
+    return { name, job, clicked, info}
+  }
+}
+</script>
