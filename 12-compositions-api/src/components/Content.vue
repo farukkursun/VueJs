8a8d3d834118689252c83e2ff7d9750005c37365
faruk <kursunomerfaruk2@gmail.com>
2023-04-18 17:07:30 +0200

    setup props

---
+++ 12-compositions-api/src/components/Content.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+     <div v-for="con in content" :key="con.id">
+        {{con.title}}
+
+  </div>
+</div>
+ 
+</template>
+
+<script>
+export default {
+    props: ['content'],
+    setup(props) {
+        console.log(props.content);
+        
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- 12-compositions-api/src/views/HomeView.vue
+++ 12-compositions-api/src/views/HomeView.vue
@@ -1,44 +1,54 @@
 <template>
   <div class="home">
-    <input type="text" v-model="search">
+    <!-- <input type="text" v-model="search">
     <div v-for="day in findet" :key='day'>
       {{day}}
 
     </div>
-    <button @click="stop">Stop</button>
-    
+    <button @click="stop">Stop</button> -->
+    <Content :content="content"/>
+
   </div>
 </template>
 
 <script>
 import {ref, computed, watch, watchEffect} from 'vue';
-
+import Content from '../components/Content.vue'
 
 export default {
   name: 'HomeView',
   components: {
+    Content
     
   },
 
   setup() {
-   const days = ref(['pazartesi', 'sali', 'carsamba', 'persembe', 'cuma']) 
-   const search = ref('')
-   const findet = computed(()=>{
-    return days.value.filter((day)=> day.includes(search.value))
-   })
-   const watchs = watch(search, ()=>{
-    console.log('watch calisti');
-   })
-   const watcheffects = watchEffect(()=>{
-    console.log('wawatch effect calisti', days.value);
-    console.log('watch effect yeniden calisti',search.value);
-   })
-   const stop = ()=>{
-    watchs()
-    watcheffects()
-   }
+  //  const days = ref(['pazartesi', 'sali', 'carsamba', 'persembe', 'cuma']) 
+  //  const search = ref('')
+  //  const findet = computed(()=>{
+  //   return days.value.filter((day)=> day.includes(search.value))
+  //  })
+  //  const watchs = watch(search, ()=>{
+  //   console.log('watch calisti');
+  //  })
+  //  const watcheffects = watchEffect(()=>{
+  //   console.log('wawatch effect calisti', days.value);
+  //   console.log('watch effect yeniden calisti',search.value);
+  //  })
+  //  const stop = ()=>{
+  //   watchs()
+  //   watcheffects()
+  //  }
    
-   return {days, findet, search,stop}
+  //  return {days, findet, search,stop}
+
+  const content = ref([
+    {id:1, title:'deneme 1', details:'detail 1'},
+    {id:2, title:'deneme 2', details:'detail 2'},
+    {id:3, title:'deneme 3', details:'detail 3'},
+    {id:4, title:'deneme 4', details:'detail 4'}
+    ])
+    return {content}
     
     }
     
